53328e5342cd473f4ff88cbbc35f4fdb4d694ac8
ITurres <emanueliturres.co@gmail.com>
2024-01-16 20:52:28 -0300

    Feat: Configure CORS for 'http://localhost:3000' (front-end-port) in Rails

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,16 @@ module HelloRailsBackEnd
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "http://localhost:3000"
+        resource "/api/v1/*",
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head],
+          expose: ["Access-Token", "Uid", "Client"],
+          credentials: true
+      end
+    end
   end
 end
