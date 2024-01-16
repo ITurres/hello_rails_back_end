60a8474b8f3239571fbc384d474e40dcb95ecea7
ITurres <emanueliturres.co@gmail.com>
2024-01-16 18:31:24 -0300

    Feat: Update seeds with faker data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "faker"
+
+# Create 5 different greetings
+
+5.times do
+  Greeting.create(message: "Hello #{Faker::Name.name}!")
+end
