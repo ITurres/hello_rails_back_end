1dd66dca8b302cba9c71e1dc898d858939508401
ITurres <emanueliturres.co@gmail.com>
2024-01-16 18:29:56 -0300

    Feat: update routes for 'Api/v1/greetings' resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :greetings
+  namespace :api do
+    namespace :v1 do
+      get 'greetings/random', to: 'greetings#random', defaults: { format: :json }
+      resources :greetings, only: [:show]
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
